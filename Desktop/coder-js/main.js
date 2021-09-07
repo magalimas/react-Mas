8b9975dac32ec295e52da5e46b2969ddae0fb059
Magali Mas <magga.mas@hotmail.com>
2021-09-07 00:46:43 -0300

    primerClaseJs

---
+++ Desktop/coder-js/main.js
@@ -0,0 +1,17 @@
+let nombre = prompt ('Escriba su nombre');
+
+alert('Bienvenido' + " " + nombre);
+
+let fav = prompt ('Introduzca su numero favorito');
+
+let numero = parseInt(fav);
+
+let diez = 10;
+
+console.log(numero + diez);
+
+let pais = prompt ('Introduzca su pais');
+
+let barrio = prompt ('Introduzca su barrio');
+
+alert('Tu vives en ' + pais + ", " + barrio);
